0945ae191c006944ec1697ffee3db839940dc378
Chelsea Spangler <chelseanspangler@gmail.com>
2020-05-09 13:43:39 -0700

    aliases only render if aliases exist

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { GoTProvider } from '../../hooks/GoTProvider.jsx';
 import Header from '../Header/Header.jsx';
 import Characters from './Characters/Characters.jsx';
 
 export default function App() {
   return (
-    <GoTProvider>
+    <>
       <Header />
       <Characters />
-    </GoTProvider>
+    </>
   );
 }
