9f047d7fdf452969cfebd39db718a4df0050f0ee
Chelsea Spangler <chelseanspangler@gmail.com>
2020-05-09 21:28:01 -0700

    got rid of side margins

--- src/components/Character/Character.jsx
+++ src/components/Character/Character.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from '../App/App.css';
 
 const Character = ({ name = 'unknown', culture, titles, aliases }) => {
   const aliasElements = aliases.map((alias, i) => 
@@ -7,12 +8,12 @@ const Character = ({ name = 'unknown', culture, titles, aliases }) => {
   );
   
   return (
-    <>
+    <section className={styles.card}>
       {name ? <h3>Name: {name}</h3> : <h3>{aliases[0]}</h3>}
       {culture !== '' && <h4>Culture: {culture}</h4>}
       {titles[0] && <h4>Title: {titles[0]}</h4>}
       {aliases.length > 1 && <ul>Aliases: {aliasElements}</ul>}
-    </>
+    </section>
   );};
 
 Character.propTypes = {
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import { useToggle } from '../../hooks/GoTProvider';
 const Header = () => {
   const toggle = useToggle();
   return (
-    <header>
+    <header className={Header}>
       <h1>Game of Thrones Characters</h1>
       <input type="checkbox" id="toggle" onChange={toggle} />
       <label htmlFor="toggle">Fire/Ice</label>
